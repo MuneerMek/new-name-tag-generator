1d63c59917beff9c7f0a0fe3b8230b5cfbcb8834
debian-pc <mmekraz781@proton.me>
2023-12-20 17:31:35 -0500

    Added UserInput.js component

---
+++ src/UserInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  state = { name: "" };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Add a new name here..."
+          value={this.state.name}
+        />
+        <input type="submit" value="Create Name Tag" />
+      </form>
+    );
+  }
+}
+
+export default UserInput;
